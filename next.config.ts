d503c9a854f1aa943eb904d5ccda6da3845d0160
Miteek2000 <243748@ids.upchiapas.edu.mx>
2026-03-12 22:13:02 -0600

    modificacion para imagenes

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,8 @@ const nextConfig: NextConfig = {
   unoptimized: process.env.NODE_ENV === 'development',
   remotePatterns: [
     {
-      protocol: 'http',
-      hostname: 'localhost',
+      protocol: 'https',
+      hostname: 'hc-backend.duckdns.org',
       port: '3001',
       pathname: '/uploads/**',
     },
