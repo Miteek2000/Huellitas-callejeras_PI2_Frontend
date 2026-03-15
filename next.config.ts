4d1a9e3290f69908f39112349d54559acc5c02d8
pao243685 <243685@ids.upchiapas.edu.mx>
2026-03-14 20:38:59 -0600

    configuracion para imagenes con protocolo https

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- images: {
-  unoptimized: process.env.NODE_ENV === 'development',
-  remotePatterns: [
-    {
-      protocol: 'https',
-      hostname: 'hc-backend.duckdns.org',
-      port: '3001',
-      pathname: '/uploads/**',
-    },
-  ],
-},
+  output: 'standalone',
+  images: {
+    unoptimized: process.env.NODE_ENV === 'development',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'hc-backend.duckdns.org',
+        port: '3001',
+        pathname: '/uploads/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
-
