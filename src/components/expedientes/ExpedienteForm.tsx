1571827293553bd2be76280c0237e568b204ca89
Miteek2000 <243748@ids.upchiapas.edu.mx>
2026-03-24 02:30:31 -0600

    detalles minimos

--- src/components/expedientes/ExpedienteForm.tsx
+++ src/components/expedientes/ExpedienteForm.tsx
@@ -280,10 +280,10 @@ export const ExpedienteForm: React.FC<ExpedienteFormProps> = ({
         )}
 
         <div className="flex flex-wrap justify-center gap-4 mt-16">
-          <Button type="submit" variant="primary" disabled={readOnly || isSaving} className="!bg-[#2B264F] !text-white w-full sm:w-auto sm:px-16 md:px-24 h-10 flex items-center justify-center font-semibold">
+          <Button type="submit" variant="primary" disabled={readOnly || isSaving} className="!bg-[#2B264F] !text-white w-full sm:w-auto px-16 md:px-50 h-10 flex items-center justify-center font-semibold">
             {isSaving ? 'Guardando...' : 'Guardar'}
           </Button>
-          <Button type="button" variant="secondary" onClick={handleCancel} className="!bg-[#A7A7A7] !text-white w-full sm:w-auto sm:px-16 md:px-24 h-10 flex items-center justify-center font-semibold">
+          <Button type="button" variant="secondary" onClick={handleCancel} className="!bg-[#A7A7A7] !text-white w-full sm:w-auto px-16 md:px-50 h-10 flex items-center justify-center font-semibold">
             Cancelar
           </Button>
         </div>
